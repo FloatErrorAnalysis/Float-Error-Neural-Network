5eb112592684366c6ad7d5a475d87ee637f12f94
2529716798 <2529716798@qq.com>
2018-11-19 18:10:25 +0800

    auto generate

--- DataGenerator.py
+++ DataGenerator.py
@@ -3,11 +3,22 @@ import os
 
 class DataGenerator:
     CMD0 = 'cd dataset'
-    CMD1 = 'clang -S -emit-llvm dataset/sqrt_minus.cpp'
+    CMD1 = 'clang -S -emit-llvm /tmp/pycharm_project/tmp/LLVM/cpp_files/'
+    CMD2 = ' -S -o /tmp/pycharm_project/tmp/LLVM/dataset/'
 
     def generate_ll(self):
-        os.system(self.CMD1)
-        os.system(self.CMD)
+        for cpp in self.list_file_names():
+            cmd = self.CMD1 + cpp + self.CMD2 + cpp.split('.')[0] + '.ll'
+            os.system(cmd)
+
+    @staticmethod
+    def list_file_names():
+        names = []
+        for root, dirs, files in os.walk('cpp_files'):
+            for file in files:
+                if os.path.splitext(file)[1] == '.cpp':
+                    names.append(file)
+        return names
 
 
 generator = DataGenerator()
